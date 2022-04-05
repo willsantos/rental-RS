6bfacc13048e43703c2d80ec9b6b25eeae75304f
Wilson Santos <contato@wilsonsantos.net>
2022-04-05 16:25:05 -0300

    :sparkles: feature: Add Create Category Service
    Remove create category responsability from route.
    Create a service with SRP (create a category).
    Update route to use Create Category Service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,17 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  createCategoryService.execute({ name, description });
 
-  if (categoryAlreadyExists) {
-    return response.status(500).json({ error: "Category Already Exists" });
-  }
-
-  categoriesRepository.create({ name, description });
   return response.status(201).send();
 });
 
